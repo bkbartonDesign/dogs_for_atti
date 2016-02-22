b705d63d753535de004441d779bf56c3b7e0f6d7
apgregory <aaron.p.gregory@gmail.com>
2016-02-22 15:19:17 -0600

    inits and renders slider

--- app.js
+++ app.js
@@ -2,8 +2,42 @@
   $.post(
     {url:"mock.php"}
   ).success(function(res){
-    console.log(res);
+    init(res);
   }).error(function(err){
     console.log("error ",err);
   })
+
+  function init(res){
+    var instagramArray = JSON.parse(res).data;
+    var sliderImagesHtml = imageListItemHtml(instagramArray);
+    renderSlider(sliderImagesHtml);
+  }
+
+  function renderSlider(sliderImagesHtml){
+    var $slider = $('<div id="dog-carousel">').append(sliderImagesHtml);
+    $('body').append($slider);
+    $slider.slick();
+  }
+
+  function imageListItemHtml(unfilteredArray, currentHtmlStirng){
+    var html = currentHtmlStirng || ''
+
+    if (
+        unfilteredArray[0].type === "image" &&
+        unfilteredArray[0].images &&
+        unfilteredArray[0].images.standard_resolution &&
+        unfilteredArray[0].images.standard_resolution.url
+      ) {
+      html += '<li><image src="' + unfilteredArray[0].images.standard_resolution.url + '"></li>';
+    }
+
+    var remainingArray = unfilteredArray.slice(1);
+
+    if (remainingArray.length) {
+      return imageListItemHtml(remainingArray, html);
+    } else {
+      return html;
+    }
+  }
+
 })();
--- mock.php
+++ mock.php
@@ -42,7 +42,7 @@ $res = '{
 				"height": 150
 			},
 			"standard_resolution": {
-				"url": "http://distillery.s3.amazonaws.com/media/2011/02/02/f9443f3443484c40b4792fa7c76214d5_7.jpg",
+				"url": "http://i.huffpost.com/gen/3030612/thumbs/r-DOG-403xFB.jpg",
 				"width": 612,
 				"height": 612
 			}
@@ -50,6 +50,54 @@ $res = '{
 		"id": "22699663",
 		"location": null
 	}, {
+		"type": "image",
+		"users_in_photo": [],
+		"filter": "Earlybird",
+		"tags": ["snow"],
+		"comments": {
+			"count": 3
+		},
+		"caption": {
+			"created_time": "1296703540",
+			"text": "#Snow",
+			"from": {
+				"username": "emohatch",
+				"id": "1242695"
+			},
+			"id": "26589964"
+		},
+		"likes": {
+			"count": 1
+		},
+		"link": "http://instagr.am/p/BWl6P/",
+		"user": {
+			"username": "emohatch",
+			"profile_picture": "http://distillery.s3.amazonaws.com/profiles/profile_1242695_75sq_1293915800.jpg",
+			"id": "1242695",
+			"full_name": "Dave"
+		},
+		"created_time": "1296703536",
+		"images": {
+			"low_resolution": {
+				"url": "http://distillery.s3.amazonaws.com/media/2011/02/02/f9443f3443484c40b4792fa7c76214d5_6.jpg",
+				"width": 306,
+				"height": 306
+			},
+			"thumbnail": {
+				"url": "http://distillery.s3.amazonaws.com/media/2011/02/02/f9443f3443484c40b4792fa7c76214d5_5.jpg",
+				"width": 150,
+				"height": 150
+			},
+			"standard_resolution": {
+				"url": "http://i.huffpost.com/gen/3030612/thumbs/r-DOG-403xFB.jpg",
+				"width": 612,
+				"height": 612
+			}
+		},
+		"id": "22699663",
+		"location": null
+	},
+	{
 		"type": "video",
 		"videos": {
 			"low_resolution": {
